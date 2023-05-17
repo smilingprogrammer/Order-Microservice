e05b49d3d815712b1006a0ea05763d4c74df60e2
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2023-05-18 00:17:54 +0100

    Completed the logic and resolved bugs

---
+++ src/main/java/com/example/Order/Microservice/logic/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.example.Order.Microservice.logic;
+
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "order", path = "order")
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>, CrudRepository<Order, Long> {
+
+    @Query("SELECT max(o.updated) FROM Order o")
+    Date lastUpdate();
+
+}
